7d4ea5ba6ac25b280d09a2c4e001693f45150a6c
aryamanarora <aryaman.arora2020@gmail.com>
2016-09-25 14:15:32 -0400

    date fix

--- js/lineage.js
+++ js/lineage.js
@@ -5,7 +5,7 @@ function start() {
   var width = window.innerWidth / zoomLevel,
       height = window.innerHeight / zoomLevel;
   var fill = d3.scale.category20();
-  var startYear = 1900;
+  var startYear = 1938;
   var currentYear = startYear;
   var lastYear = 2016;
   var timeVector = 0;

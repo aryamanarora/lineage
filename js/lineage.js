488cbaa50f013025a93924d98283b31e1471f418
aryamanarora <aryaman.arora2020@gmail.com>
2016-09-25 18:18:56 -0400

    try something else out

--- js/lineage.js
+++ js/lineage.js
@@ -165,7 +165,7 @@ function start() {
     node.enter()
         .insert("circle")
         .attr("class", "node")
-        .attr("r", (currentYear - function(d))/3)
+        .attr("r", (currentYear - function(d) { return d.value / 3; })
         .on("mouseover", function(d) {
 
           d3.select(this).transition().duration(100).attr('r',10);
